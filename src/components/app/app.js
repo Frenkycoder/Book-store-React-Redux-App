038fb245177ab3ff2f5f7ae9b4882b44b24cdb91
Andrew Michaylov <andrewloveua@gmail.com>
2021-08-11 13:58:13 +0300

    Create book-list and book-list item components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,16 @@
 import React from "react";
-import "./app.css";
+import { Route, Switch } from "react-router-dom";
+import { HomePage, CartPage } from "../pages";
 
+import "./app.css";
 const App = () => {
-  return <div>App</div>;
+  return (
+    // Switch - робить так,  якщо один з внутрішніх роутів спрацьовував, інші ігноруються.
+    <Switch>
+      <Route path="/" component={HomePage} exact />
+      <Route path="/cart" component={CartPage} />
+    </Switch>
+  );
 };
 
 export default App;
